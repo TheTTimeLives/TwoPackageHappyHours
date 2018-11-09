f7ff683dae7bd23fddb5c8ddb64e4ea78bfa1bcb
TheTTimeLives <37144865+TheTTimeLives@users.noreply.github.com>
2018-11-09 16:38:55 -0500

    Added links to media card

--- client/src/components/Card/MediaCard.js
+++ client/src/components/Card/MediaCard.js
@@ -24,37 +24,50 @@ const actionWidth = {
 
 function MediaCard(props) {
   const { classes } = props
+  var urladd1 = props.currentTab.address1.split(' ').join('+');
+  var urladd2 = props.currentTab.address2.split(' ').join('+');
+  var urladd3 = props.currentTab.address3.split(' ').join('+');
+
+  //If the third address includes the city/state data, change the url2 data to this...
+  if (props.currentTab.address3.includes(',')) {
+    urladd2 = props.currentTab.address3.split(' ').join('+');
+  }
+
+
+  let mapsLink = `https://www.google.com/maps/place/${urladd1}+${urladd2}`
   return (
     <Card className={classes.card}>
-      <CardActionArea
+      {/* <CardActionArea
         disableTypography
         style = {actionWidth}
-      >
-        <CardMedia
-          className={classes.media}
-          image={props.currentTab.image}
-          
-        />
-        <CardContent>
-          <Typography gutterBottom variant='h5' component='h2' style = {{fontSize: 30}}>
-            {props.currentTab.name}
-          </Typography>
-          {console.log('Media Card props below')}
-          {console.log(props)}
-          <Typography component='p' style = {{fontSize: 17}}>
+      > */}
+      <CardMedia
+        className={classes.media}
+        image={props.currentTab.image}
+
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='h2' style={{ fontSize: 30 }}>
+          {props.currentTab.name}
+        </Typography>
+        {console.log('Media Card props below')}
+        {console.log(props)}
+        <Typography component='p' style={{ fontSize: 17 }}>
+          <a style={{ color: 'black' }} href={mapsLink}>
             {props.currentTab.address1}
             <br />
             {props.currentTab.address2}
             <br />
             {props.currentTab.address3}
-            {/* <br /> */}
-            Rating : {props.currentTab.rating} · {props.currentTab.reviewCount} reviews
-            
-            {/* Reviews : {props.currentTab.reviewCount} */}
+          </a>
 
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+          {/* <br /> */}
+          <a style={{ color: 'black' }} href={props.currentTab.url}>Rating : {props.currentTab.rating} · {props.currentTab.reviewCount} reviews</a>
+          {/* Reviews : {props.currentTab.reviewCount} */}
+
+        </Typography>
+      </CardContent>
+      {/* </CardActionArea> */}
       <CardActions>
         {/* <Button size="small" color="primary">
           Share
